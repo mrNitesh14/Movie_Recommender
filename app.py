1719da3bd5d580c1870ec7319edf1c44a3e0a6c6
Adii2202 <adiningule2@gmail.com>
2024-11-22 14:25:16 +0530

    fast api

--- app.py
+++ app.py
@@ -4,12 +4,24 @@ from fastapi import FastAPI, HTTPException
 from typing import List
 import bcrypt
 import pandas as pd
+from fastapi.staticfiles import StaticFiles
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
 
 # Initialize FastAPI app
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Update this to the frontend URL in production
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 import sqlite3
 import csv
@@ -252,12 +264,20 @@ def signup(username: str, password: str):
     
     return {"message": "User created successfully!"}
 
+from pydantic import BaseModel
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+
 @app.post("/login/")
-def login(username: str, password: str):
+def login(request: LoginRequest):
     """
     User login: Validate username and password.
     """
     # Fetch the user from the database
+    username = request.username
+    password = request.password
     stored_user = execute_query("SELECT id, password_hash FROM users WHERE username = ?", 
                                 (username,), fetch_one=True)
     
@@ -269,7 +289,9 @@ def login(username: str, password: str):
     if not verify_password(password, hashed_password):
         raise HTTPException(status_code=400, detail="Invalid username or password")
     
-    return {"message": f"User {username} logged in successfully!", "user_id": user_id}
+    return RedirectResponse(url="/static/landingpage.html", status_code=302)
+
+
 
 @app.post("/users/{user_id}/favorite_genre/")
 def update_favorite_genre(user_id: int, favorite_genres: List[str]):
